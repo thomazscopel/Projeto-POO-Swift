3809ae8dcc5bd611b382cf2f11ac20dba268867d
Gabriel Spinola <134285947+spinolagab@users.noreply.github.com>
2025-09-08 17:59:45 +0000

    feat: "Classe pessoa adicionada e testada com sucesso"

--- Sources/Projeto/main.swift
+++ Sources/Projeto/main.swift
@@ -1,3 +1,20 @@
 import Foundation
 
-print("Aula 1 de Swift!!")
+var teste = Pessoa(nome: "Julio", email: "teste@teste.com")
+
+print(teste.getNome())
+print(teste.getEmail())
+
+// Nao funciona por ser private
+// teste.setEmail(email: "falhou")
+// teste.setNome(nome: "falhou")
+
+print(teste.getNome())
+print(teste.getEmail())
+
+
+teste.alterarEmail(email: "teste@sucesso.com", senha: 1234)
+teste.alterarNome(nome: "Julio Cesar", senha: 1234)
+
+print(teste.getNome())
+print(teste.getEmail())
