e309a91300919d1632414250b2e76b174f37fcea
Gabriel <112496072+GabrielNS021@users.noreply.github.com>
2025-09-09 17:39:17 +0000

    09/09

--- Sources/Projeto/main.swift
+++ Sources/Projeto/main.swift
@@ -1,3 +1,3 @@
-let mAula = Aula(nome: "Joao", Instrutor(nome: "Joao Instrutor", email: "instrutor@email.com", especialidade: "especial"))
+let mAula = Aula(nome: "Joao", instrutor: Instrutor(nome: "Joao Instrutor", email: "instrutor@email.com", especialidade: "especial"))
 
 print(mAula.getDescricao())
