263bb8bf384602bab21f98c67d73467f3a2e8dae
Thomaz de Souza Scopel <106708501+thomazscopel@users.noreply.github.com>
2025-09-10 17:37:40 +0000

    Classe Academia

--- Sources/Projeto/Dia 3/Academia.swift
+++ Sources/Projeto/Dia 3/Academia.swift
@@ -1,12 +1,20 @@
 class Academia {
-    private var nome: String
+    private let nome: String
     private var alunosMatriculados: [String: Aluno]
     private var instrutoresContratados: [String: Instrutor]
     private var aparelhos: [Aparelho]
     private var aulasDisponiveis: [Aula]
 
-    init(nome: String) {
+    init(nome: String,
+    alunosMatriculados: [String: Aluno] = [:],
+    instrutoresContratados: [String: Instrutor] = [:],
+    aparelhos: [Aparelho] = [],
+    aulasDisponiveis: [Aula] = []) {
         self.nome = nome
+        self.alunosMatriculados = alunosMatriculados
+        self.instrutoresContratados = instrutoresContratados
+        self.aparelhos = aparelhos
+        self.aulasDisponiveis = aulasDisponiveis
     }
 
 
