dce8fa88b839994778aebf84b9ac4e2410c816b7
Gabriel <112496072+GabrielNS021@users.noreply.github.com>
2025-09-10 17:16:36 +0000

    10/09

---
+++ Sources/Projeto/Dia 3/Academia.swift
@@ -0,0 +1,13 @@
+class Academia {
+    private var nome: String
+    private var alunosMatriculados: [String: Aluno]
+    private var instrutoresContratados: [String: Instrutor]
+    private var aparelhos: [Aparelho]
+    private var aulasDisponiveis: [Aula]
+
+    init(nome: String) {
+        self.nome = nome
+    }
+
+
+}
