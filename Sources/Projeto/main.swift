fe61f22158cd2210d498d5b4116970e332e244b6
Gabriel Spinola <134285947+spinolagab@users.noreply.github.com>
2025-09-10 19:02:30 +0000

    "testes efetuados"

--- Sources/Projeto/main.swift
+++ Sources/Projeto/main.swift
@@ -4,19 +4,23 @@ print(mAula.getDescricao())
 
 var planoAluno = PlanoAnual()
 
+var instrutorTeste = Instrutor(nome: "Joao", email: "abc@def.com", especialidade: "Cardio")
 var alunoTeste = Aluno(nome: "Nome", email: "teste@teste.com", matricula: "ASDFGHJKLÑ", nivel: .iniciante, plano: planoAluno)
-
+var aparelhoTeste = Aparelho(nomeItem: "sla")
 // var alunosMatriculados = [alunoTeste.matricula : alunoTeste]
 
-var academia = Academia(nome: "Nome")
+var academia = Academia(nome: "Nome", alunosMatriculados: [alunoTeste.matricula : alunoTeste],
+    instrutoresContratados: [instrutorTeste.email: instrutorTeste],
+    aparelhos: [aparelhoTeste],
+    aulasDisponiveis: [mAula])
 
-academia.buscarAluno(porMatricula: alunoTeste.matricula)
+print(academia.buscarAluno(porMatricula: alunoTeste.matricula)!)
 
 academia.listarAlunos()
 
-academia.matricularAluno(Aluno)
+academia.matricularAluno(alunoTeste)
 
-academia.buscarAluno(porMatricula: alunoTeste.matricula)
+print(academia.buscarAluno(porMatricula: alunoTeste.matricula)!)
 
 academia.listarAlunos()
 
